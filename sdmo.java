a8d19f668bd7e513578927a60add285e4fbd1216
phrzdgal <phiroze.khattra@gmail.com>
2022-02-04 09:32:28 -0800

    libs created, failing test created

--- sdmo.java
+++ sdmo.java
@@ -1,1 +1,5 @@
-public class sdmo {}
+public class sdmo {
+    public static int subtraction(int n1, int n2) {
+        return n1 - n2;
+    }
+}
---
+++ sdmotester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class sdmotester {
+    @Test
+    public void subtracton() {
+        assertEquals(2, sdmo.subtraction(5, 1)); 
+    }
+}
